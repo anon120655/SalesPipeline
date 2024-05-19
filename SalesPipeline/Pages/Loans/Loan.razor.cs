6b1638498d41a793277127a0c5eb58570ffc052a
Anon <anon.non_@hotmail.com>
2024-05-19 19:52:59 +0700

    23

--- SalesPipeline/Pages/Loans/Loan.razor.cs
+++ SalesPipeline/Pages/Loans/Loan.razor.cs
@@ -1,5 +1,10 @@
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using SalesPipeline.Shared.Modals;
 using SalesPipeline.Utils;
 using SalesPipeline.Utils.Resources.Authorizes.Users;
+using SalesPipeline.Utils.Resources.Loans;
+using SalesPipeline.Utils.Resources.Shares;
 
 namespace SalesPipeline.Pages.Loans
 {
@@ -7,12 +12,127 @@ namespace SalesPipeline.Pages.Loans
 	{
 		string? _errorMessage = null;
 		private User_PermissionCustom _permission = new();
+		private allFilter filter = new();
+		private LookUpResource LookUp = new();
+		private List<LoanCustom>? Items;
+		public Pager? Pager;
+
+		ModalConfirm modalConfirm = default!;
 
 		protected override async Task OnInitializedAsync()
 		{
 			_permission = UserInfo.User_Permissions.FirstOrDefault(x => x.MenuNumber == MenuNumbers.Loan) ?? new User_PermissionCustom();
 			StateHasChanged();
 			await Task.Delay(1);
+		}
+
+		protected async override Task OnAfterRenderAsync(bool firstRender)
+		{
+			if (firstRender)
+			{
+				await SetInitManual();
+				await Task.Delay(1);
+
+				await SetQuery();
+				StateHasChanged();
+
+				firstRender = false;
+			}
+		}
+
+		protected async Task SetInitManual()
+		{
+			StateHasChanged();
+			await Task.Delay(1);
+		}
+
+		protected async Task SetQuery(string? parematerAll = null)
+		{
+			string uriQuery = _Navs.ToAbsoluteUri(_Navs.Uri).Query;
+
+			if (parematerAll != null)
+				uriQuery = $"?{parematerAll}";
+
+			filter.SetUriQuery(uriQuery);
+
+			await SetModel();
+			StateHasChanged();
+		}
+
+		protected async Task SetModel()
+		{
+			filter.userid = UserInfo.Id;
+			var data = await _loanViewModel.GetList(filter);
+			if (data != null && data.Status)
+			{
+				Items = data.Data?.Items;
+				Pager = data.Data?.Pager;
+				if (Pager != null)
+				{
+					Pager.UrlAction = "/loan";
+				}
+			}
+			else
+			{
+				_errorMessage = data?.errorMessage;
+				_utilsViewModel.AlertWarning(_errorMessage);
+			}
+
+			StateHasChanged();
+		}
+
+		protected async Task OnSelectPagesize(int _number)
+		{
+			Items = null;
+			StateHasChanged();
+			filter.page = 1;
+			filter.pagesize = _number;
+			await SetModel();
+			_Navs.NavigateTo($"{Pager?.UrlAction}?{filter.SetParameter(true)}");
+		}
+
+		protected async Task OnSelectPage(string parematerAll)
+		{
+			await SetQuery(parematerAll);
+			StateHasChanged();
+		}
+
+		protected async Task ConfirmDelete(string? id, string? txt)
+		{
+			await modalConfirm.OnShowConfirm(id, $"คุณต้องการลบข้อมูล <span class='text-primary'>{txt}</span>");
+		}
+
+		protected async Task Delete(string id)
+		{
+			await modalConfirm.OnHideConfirm();
+
+			var data = await _loanViewModel.DeleteById(new UpdateModel() { id = id, userid = UserInfo.Id });
+			if (data != null && !data.Status && !String.IsNullOrEmpty(data.errorMessage))
+			{
+				_errorMessage = data?.errorMessage;
+				_utilsViewModel.AlertWarning(_errorMessage);
+			}
+			await SetModel();
+		}
+
+		protected async Task StatusChanged(ChangeEventArgs e, Guid id)
+		{
+			if (e.Value != null && Boolean.TryParse(e.Value.ToString(), out bool val))
+			{
+				var data = await _loanViewModel.UpdateStatusById(new UpdateModel() { id = id.ToString(), userid = UserInfo.Id, value = val.ToString() });
+				if (data != null && !data.Status && !String.IsNullOrEmpty(data.errorMessage))
+				{
+					_errorMessage = data?.errorMessage;
+					_utilsViewModel.AlertWarning(_errorMessage);
+				}
+				else
+				{
+					string? actiontxt = val ? "<i class=\"fa-regular fa-circle-check\"></i> เปิด" : "<i class=\"fa-solid fa-circle-xmark\"></i> ปิด";
+					string fulltxt = $"{actiontxt}การใช้งานเรียบร้อย";
+					await _jsRuntimes.InvokeVoidAsync("SuccessAlert", fulltxt);
+					await SetModel();
+				}
+			}
 		}
 
 
