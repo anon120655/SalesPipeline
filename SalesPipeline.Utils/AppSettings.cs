40d0fa62fa837a4f5cc4cf8a85a80e61a10877b3
Anon <anon.non_@hotmail.com>
2024-06-04 20:42:02 +0700

    GetCalByAppBusId

--- SalesPipeline.Utils/AppSettings.cs
+++ SalesPipeline.Utils/AppSettings.cs
@@ -5,7 +5,7 @@ namespace SalesPipeline.Utils
 	public class AppSettings
 	{
 		public string? Secret { get; set; }
-		public string? SiteUpdate { get; set; } = $"2567-06-04";
+		public string? SiteUpdate { get; set; } = $"2567-06-05";
 		public string ServerSite { get; set; } = null!;
 		public string? baseUriApi { get; set; }
 		public string? baseUriWeb { get; set; }
