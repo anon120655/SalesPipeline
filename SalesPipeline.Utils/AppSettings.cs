4d5926b3d1455b52f968b94de7a0644d619f76b9
Anon <anon.non_@hotmail.com>
2024-08-05 11:52:09 +0700

    kkk

--- SalesPipeline.Utils/AppSettings.cs
+++ SalesPipeline.Utils/AppSettings.cs
@@ -5,7 +5,7 @@ namespace SalesPipeline.Utils
 	public class AppSettings
 	{
 		public string? Secret { get; set; }
-		public string? SiteUpdate { get; set; } = $"2567-08-04";
+		public string? SiteUpdate { get; set; } = $"2567-08-05";
 		public string ServerSite { get; set; } = null!;
 		public string? baseUriApi { get; set; }
 		public string? baseUriWeb { get; set; }
