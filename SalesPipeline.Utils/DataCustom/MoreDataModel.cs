3f06f9deef4e2c0181588c7bd34ce1f3de94666b
Anon <anon.non_@hotmail.com>
2025-06-16 15:25:25 +0700

    900000

--- SalesPipeline.Utils/DataCustom/MoreDataModel.cs
+++ SalesPipeline.Utils/DataCustom/MoreDataModel.cs
@@ -54,7 +54,7 @@ namespace SalesPipeline.Utils.DataCustom
 				{
 					phoenixModel.Add(new Sale_PhoenixCustom
 					{
-						cif_no = $"T000{i}",
+						cif_no = $"900000{i}",
 						cif_name = $"บริษัท 00{i}_{j}",
 						status_type = $"วิเคราะห์โครงการ00{i}_{j}",
 						status_code = $"100 - RM วิเคราะห์โครงการ{i}_{j}",
